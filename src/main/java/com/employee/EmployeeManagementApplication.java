46a01245049a7117d4d6b9f8260fd44274f565cb
Yash kumar Dubey <ydubey020@gmail.com>
2025-01-09 12:35:34 +0530

    Update EmployeeManagementApplication.java
    changes in code 
    Extend SpringBootServletInitializer: This allows your Spring Boot application to be deployed as a WAR in an external Tomcat server.
    Override configure method: This method is overridden to specify the main application class when the application starts in an external container.

--- src/main/java/com/employee/EmployeeManagementApplication.java
+++ src/main/java/com/employee/EmployeeManagementApplication.java
@@ -3,13 +3,19 @@ package com.employee;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EmployeeManagementApplication {
-	  private static final Logger logger = Logger.getLogger(EmployeeManagementApplication.class);
+public class EmployeeManagementApplication extends SpringBootServletInitializer {
+    private static final Logger logger = Logger.getLogger(EmployeeManagementApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmployeeManagementApplication.class);
+    }
 }
